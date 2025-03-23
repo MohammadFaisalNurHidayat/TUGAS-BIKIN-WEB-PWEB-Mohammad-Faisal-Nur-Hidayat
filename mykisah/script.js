226b1df87ebe13c7c53a5953d0a5c25e3d8e8f2e
MohammadFaisalNurHidayat <149564186+MohammadFaisalNurHidayat@users.noreply.github.com>
2025-03-23 23:20:50 +0700

    Add files via upload

---
+++ mykisah/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let btn = document.getElementById("btnClick");
+    let image = document.getElementById("mainImage");
+    let text = document.querySelector(".content p"); 
+
+    let isOriginal = true;
+
+    btn.addEventListener("click", function() {
+        if (isOriginal) {
+            image.src = "images/image2.jpg";
+            text.innerText = "LU SESUDAH BEFORE TEKAN TOMBOL.";
+        } else {
+            image.src = "images/image.jpg";
+            text.innerText = "LU SEBELUM BEFORE TEKAN TOMBOL.";
+        }
+        isOriginal = !isOriginal; 
+    });
+});
